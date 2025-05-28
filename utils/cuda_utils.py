4a388174f5e7f0f31560a75f97f9eed251959591
ZoeTsou <zoe.tsou.2003@gmail.com>
2025-05-28 16:15:21 +0800

    Revert "init"
    This reverts commit 15ea76a9c851b168bcd3ea0e752ce3ef00d4921a.

--- utils/cuda_utils.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-def get_available_gpu(threshold_mb=1000):
-    for i in range(torch.cuda.device_count()):
-        stats = torch.cuda.memory_reserved(i) / 1024 / 1024  # MB
-        if stats < threshold_mb:
-            return i
-    return 0  # fallback
-
